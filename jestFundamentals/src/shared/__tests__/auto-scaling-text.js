d0379d4e9fc1c7f3e892486bd8b782910b9dda3e
rafael <tbrhh@hotmail.com>
2022-06-06 15:57:35 +0100

    JestFundamentals step 7 - adding snapshop tests for auto-scaling which css is imported

--- jestFundamentals/src/shared/__tests__/auto-scaling-text.js
+++ jestFundamentals/src/shared/__tests__/auto-scaling-text.js
@@ -11,3 +11,8 @@ import {render} from '@testing-library/react'
 test('renders', () => {
   render(<AutoScalingText />)
 })
+
+test('mounts', () => {
+  const {container} = render(<AutoScalingText value="0" />)
+  expect(container.firstChild).toMatchSnapshot()
+})
